a193746945dd3da43dc01fe640de04108a648790
atal1 <pandey.atal@outlook.com>
2026-01-08 23:35:35 +0530

    solution for 1458

---
+++ Daily Question/leetCode/src/com/home/dailypractise/Leet1458.java
@@ -0,0 +1,28 @@
+package com.home.dailypractise;
+
+public class Leet1458 {
+
+    public int maxDotProduct(int[] nums1, int[] nums2) {
+        int n = nums1.length, m = nums2.length;
+        int NEG = -1_000_000_000;
+
+        int[] dp = new int[m + 1];
+        for (int j = 0; j <= m; j++) dp[j] = NEG;
+
+        for (int i = 1; i <= n; i++) {
+            int[] ndp = new int[m + 1];
+            for (int j = 0; j <= m; j++) ndp[j] = NEG;
+
+            for (int j = 1; j <= m; j++) {
+                int prod = nums1[i - 1] * nums2[j - 1];
+                int best = dp[j - 1] == NEG ? prod : Math.max(prod, dp[j - 1] + prod);
+                best = Math.max(best, dp[j]);
+                best = Math.max(best, ndp[j - 1]);
+
+                ndp[j] = best;
+            }
+            dp = ndp;
+        }
+        return dp[m];
+    }
+}
